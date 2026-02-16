28a1bd54e7a355050f8e35fcac08e4076fa34a38
azizbekqiyomov55555-dev <azizbekqiyomov55555@gmail.com>
2026-02-16 23:57:08 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # --- SOZLAMALAR ---
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+BOT_TOKEN = ("8356052924:AAG0auwyE4QSXLqI6adK8CUOfwzd7DWnihY")
+ADMIN_ID = ("8332077004", "0"))
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN topilmadi!")
