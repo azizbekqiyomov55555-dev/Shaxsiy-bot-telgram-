57b9965917597df6ab03bd0a8d6c8bbdf0095656
azizbekqiyomov55555-dev <azizbekqiyomov55555@gmail.com>
2026-02-17 01:15:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKEN = "BOT_TOKENINGIZNI_BU_YERGA_QOYING"
+TOKEN = "TOKENINGIZNI_BU_YERGA_QOYING"
 
+# Tugmalar
+keyboard = [
+    ["🟢 Sotish", "🟢 Olish"]
+]
+
+markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+# /start komandasi
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Salom 👋 Bot ishlayapti!")
+    await update.message.reply_text(
+        "Kerakli bo‘limni tanlang 👇",
+        reply_markup=markup
+    )
+
+# Tugmalarni ushlash
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+
+    if text == "🟢 Sotish":
+        await update.message.reply_text("Sotish bo‘limiga kirdingiz 💰")
+
+    elif text == "🟢 Olish":
+        await update.message.reply_text("Olish bo‘limiga kirdingiz 🛒")
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
-    print("Bot ishga tushdi...")
+    print("Bot ishlayapti...")
     app.run_polling()
 
 if __name__ == "__main__":
