dbe81a423446b2b167038c126f3d781ad1f71960
azizbekqiyomov55555-dev <azizbekqiyomov55555@gmail.com>
2026-02-17 00:52:35 +0500

    Refactor bot initialization and user greeting

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
 import os
-import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# Logging
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
-)
-
 # /start komandasi
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user_name = update.effective_user.first_name
+    user = update.effective_user.first_name
     await update.message.reply_text(
-        f"Salom {user_name}! 👋\nBotimizga xush kelibsiz!"
+        f"Salom {user}! 👋\nBot muvaffaqiyatli ishlayapti."
     )
 
-if __name__ == "__main__":
-    TOKEN = "8356052924:AAG0auwyE4QSXLqI6adK8CUOfwzd7DWnihY"
+def main():
+    TOKEN = os.getenv("BOT_TOKEN")
+
+    if not TOKEN:
+        raise RuntimeError("BOT_TOKEN topilmadi!")
 
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
 
     print("Bot ishga tushdi...")
     app.run_polling()
+
+if __name__ == "__main__":
+    main()
