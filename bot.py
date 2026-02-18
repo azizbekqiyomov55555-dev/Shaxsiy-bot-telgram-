5681d0f3b0cdf4e87e14d6e638195f1ea75d2b5c
azizbekqiyomov55555-dev <azizbekqiyomov55555@gmail.com>
2026-02-18 11:21:12 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 import asyncio
 import os
-import random
-import edge_tts
+from gtts import gTTS
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
@@ -12,46 +11,29 @@ TOKEN = "8490993231:AAEXp9bVE4DaFe47aOT8hztSUgUutw8r5Nc"
 bot = Bot(TOKEN)
 dp = Dispatcher()
 
-VOICES = [
-    "ja-JP-NanamiNeural",
-    "en-US-JennyNeural",
-    "ru-RU-DmitryNeural",
-    "ko-KR-SunHiNeural",
-    "fr-FR-DeniseNeural",
-    "de-DE-KatjaNeural",
-]
-
-# ===== start =====
 @dp.message(CommandStart())
 async def start(msg: types.Message):
-    await msg.answer("✅ Matn yubor")
+    await msg.answer("✅ Matn yubor — ovozga aylantiraman")
 
-# ===== TTS =====
 @dp.message()
 async def tts(msg: types.Message):
 
     if not msg.text:
         return
 
-    voice = random.choice(VOICES)
-    filename = f"voice_{msg.from_user.id}.mp3"
+    try:
+        filename = f"voice_{msg.from_user.id}.mp3"
 
-    for attempt in range(3):  # retry
-        try:
-            communicate = edge_tts.Communicate(msg.text, voice)
-            await communicate.save(filename)
+        tts = gTTS(text=msg.text, lang="ru")
+        tts.save(filename)
 
-            await msg.answer_voice(FSInputFile(filename))
-            os.remove(filename)
-            return
+        await msg.answer_voice(FSInputFile(filename))
+        os.remove(filename)
 
-        except Exception as e:
-            print("Retry:", attempt, e)
-            await asyncio.sleep(1)
+    except Exception as e:
+        print(e)
+        await msg.answer("❌ Ovoz yaratib bo‘lmadi")
 
-    await msg.answer("❌ Ovoz yaratib bo‘lmadi")
-
-# ===== run =====
 async def main():
     print("✅ Bot ishga tushdi")
     await dp.start_polling(bot)
